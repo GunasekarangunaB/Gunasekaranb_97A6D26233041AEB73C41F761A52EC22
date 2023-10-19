5b5f15eeb534795b2eadd831a49227c3b80f9a04
GunasekarangunaB <144666610+GunasekarangunaB@users.noreply.github.com>
2023-10-19 13:30:56 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,27 @@
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+def sort_students(student_list):
+    # Sort the list of students based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
+# Test the function with different input lists of students
+if __name__ == "__main__":
+    # Create a list of Student objects
+    students = [
+        Student("Charlie", "A101", 3.8),
+        Student("Daniel", "B102", 4.5),
+        Student("Robert David", "C103", 4.0),
+        Student("Narasiman", "D104", 3.9),
+    ]
 
-number = int(input("Enter the value :"))
-res = fact_rec(number)
+    # Sort students by CGPA in descending order
+    sorted_students = sort_students(students)
 
-print("The factorial of {} is {}.".format(number, res))
+    # Print the sorted list
+    for student in sorted_students:
+        print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
